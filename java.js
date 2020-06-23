f55f4c6d2222db8a6f5b45a402b1ea4f9437f447
Imtiaz Fahim <61643273+imtiazfahim256@users.noreply.github.com>
2020-06-23 23:35:30 +0600

    Create java.js

---
+++ java.js
@@ -0,0 +1,11 @@
+function digital_root(a,b){
+ return console.log(eval(a+b));
+}
+digital_root(-4,-9);
+
+function digital_root_1(a,b){
+    return console.log(a+b);
+   }
+   digital_root_1(-4,-9);
+   
+   
