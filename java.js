55309f591890783d2842c9db1a83e64166d8ba97
Imtiaz Fahim <61643273+imtiazfahim256@users.noreply.github.com>
2020-06-23 23:53:56 +0600

    Update java.js

--- java.js
+++ java.js
@@ -1,11 +1,11 @@
-function digital_root(a,b){
- return console.log(eval(a+b));
-}
-digital_root(-4,-9);
+ digital_root = (a, b) => {
+     return console.log(eval(a + b));
+ }
 
-function digital_root_1(a,b){
-    return console.log(a+b);
-   }
-   digital_root_1(-4,-9);
-   
-   
+
+ digital_root_1 = (a, b) => {
+     return console.log(a + b);
+ }
+
+ digital_root(-4, -9);
+ digital_root_1(-4, -9);
